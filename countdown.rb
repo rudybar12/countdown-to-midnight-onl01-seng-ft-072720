ac25ab14a8b4099e799d732982ec6bdb9e6c7b76
Rodolfo Barrientos <rudybar12@yahoo.com>
2020-07-30 08:09:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,3 +6,12 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    sleep(2)
+    x -= 1 
+  end 
+  "HAPPY NEW YEAR!"
+end 
